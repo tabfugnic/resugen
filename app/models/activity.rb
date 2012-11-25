4ea65bde5f62d0182c84d7d572b5dd0c361c91b4
Eric Collins <tabfugnic@gmail.com>
2012-11-25 09:41:49 -0600

    Move education into its own controller

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -58,10 +58,6 @@ class Activity
   
 end
 
-class Education < Activity
-  field :score, type: BigDecimal
-  field :max_score, type: BigDecimal, default: 4.0
-end
 
 class Employment < Activity
 
---
+++ app/models/education.rb
@@ -0,0 +1,6 @@
+class Education < Activity
+  field :score, type: BigDecimal
+  field :max_score, type: BigDecimal, default: 4.0
+
+
+end
