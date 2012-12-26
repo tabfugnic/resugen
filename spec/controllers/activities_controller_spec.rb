b2d16210905c92baf5c650a569db3652f3f934b4
Eric Collins <tabfugnic@gmail.com>
2012-12-26 00:58:48 -0500

    Made it so the whole activities controller spec comes back green

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -25,7 +25,7 @@ describe ActivitiesController do
 
   describe "GET new" do
     it "assigns a new activity as @activity" do
-      get :new
+      get :new, :type => [Employment, Project, Education].sample
       assigns(:activity).should be_a_new(Activity)
     end
   end
@@ -33,28 +33,37 @@ describe ActivitiesController do
   describe "GET edit" do
     it "assigns the requested activity as @activity" do
       activity = FactoryGirl.create(:activity)
+      controller.stub(:current_user).and_return(activity.user)
       get :edit, {:id => activity.to_param}
       assigns(:activity).should eq(activity)
     end
   end
 
   describe "POST create" do
+    let(:type) { [Employment, Project, Education].sample }
+    before :each do
+      @user = FactoryGirl.create(:user)
+      controller.stub(:current_user).and_return(@user)
+    end
+
     describe "with valid params" do
+      let(:valid_activity) { FactoryGirl.attributes_for(:activity) }
       it "creates a new Activity" do
         expect {
-          post :create, {:activity => valid_attributes}
-        }.to change(Activity, :count).by(1)
+          post :create, {activity: valid_activity, type: type }
+        }.to change(@user.activities, :count).by(1)
       end
 
       it "assigns a newly created activity as @activity" do
-        post :create, {:activity => valid_attributes}
+        post :create, { activity: valid_activity, type: type }
         assigns(:activity).should be_a(Activity)
         assigns(:activity).should be_persisted
       end
 
       it "redirects to the created activity" do
-        post :create, {:activity => valid_attributes} 
-        response.should redirect_to(Activity.last)
+        post :create, { activity: valid_activity, type: type} 
+        activity = @user.activities.last
+        response.should redirect_to(activity)
       end
     end
 
@@ -62,74 +71,73 @@ describe ActivitiesController do
       it "assigns a newly created but unsaved activity as @activity" do
         # Trigger the behavior that occurs when invalid params are submitted
         Activity.any_instance.stub(:save).and_return(false)
-        post :create, {:activity => {}}
+        post :create, {activity: {}, type: type }
         assigns(:activity).should be_a_new(Activity)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Activity.any_instance.stub(:save).and_return(false)
-        post :create, {:activity => {}} 
+        post :create, {activity: {}, type: type} 
         response.should render_template("new")
       end
     end
   end
 
   describe "PUT update" do
+    before :each do
+      @activity = FactoryGirl.create(:activity)
+      @user = @activity.user
+      controller.stub(:current_user).and_return(@activity.user)
+    end
     describe "with valid params" do
+      let(:valid_activity) { FactoryGirl.attributes_for(:activity) }
       it "updates the requested activity" do
-        activity = Activity.create! valid_attributes
-        # Assuming there are no other activities in the database, this
-        # specifies that the Activity created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
         Activity.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => activity.to_param, :activity => {'these' => 'params'}}
+        put :update, {:id => @activity.to_param, :activity => {'these' => 'params'}}
       end
 
       it "assigns the requested activity as @activity" do
-        activity = Activity.create! valid_attributes
-        put :update, {:id => activity.to_param, :activity => valid_attributes}
-        assigns(:activity).should eq(activity)
+        put :update, {:id => @activity.to_param, :activity => valid_activity}
+        assigns(:activity).should eq(@activity)
       end
 
       it "redirects to the activity" do
-        activity = Activity.create! valid_attributes
-        put :update, {:id => activity.to_param, :activity => valid_attributes}
-        response.should redirect_to(activity)
+        put :update, {:id => @activity.to_param, :activity => valid_activity}
+        response.should redirect_to(@activity)
       end
     end
 
     describe "with invalid params" do
       it "assigns the activity as @activity" do
-        activity = Activity.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
         Activity.any_instance.stub(:save).and_return(false)
-        put :update, {:id => activity.to_param, :activity => {}}
-        assigns(:activity).should eq(activity)
+        put :update, {:id => @activity.to_param, :activity => {}}
+        assigns(:activity).should eq(@activity)
       end
 
       it "re-renders the 'edit' template" do
-        activity = Activity.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
         Activity.any_instance.stub(:save).and_return(false)
-        put :update, {:id => activity.to_param, :activity => {}}
+        put :update, {:id => @activity.to_param, :activity => {}}
         response.should render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
+    before :each do
+      @activity = FactoryGirl.create(:activity)
+      @user = @activity.user
+      controller.stub(:current_user).and_return(@user)
+    end
+
     it "destroys the requested activity" do
-      activity = Activity.create! valid_attributes
       expect {
-        delete :destroy, {:id => activity.to_param} 
-      }.to change(Activity, :count).by(-1)
+        delete :destroy, {:id => @activity.to_param} 
+      }.to change(@user.activities, :count).by(-1)
     end
 
     it "redirects to the activities list" do
-      activity = Activity.create! valid_attributes
-      delete :destroy, {:id => activity.to_param}
+      delete :destroy, {:id => @activity.to_param}
       response.should redirect_to(activities_url)
     end
   end
