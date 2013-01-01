95b9b9167a9f7932d268e21f802afa73118ecab7
Eric Collins <tabfugnic@gmail.com>
2013-01-01 17:29:46 -0500

    Add some factories for activities

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -8,5 +8,16 @@ FactoryGirl.define do
     begin_date "2012-11-10"
     end_date "2012-11-10"
     user
+    factory :employment, :class => Employment do
+      _type Employment
+    end
+    factory :project, :class => Project do
+      _type Project
+    end
+    factory :education, :class => Education do
+      _type Education
+      score 3.5
+      max_score 4.0
+    end
   end
 end
