afc8701a84e3c4be90cebc352e8e1dcf63a60d73
Eric Collins <tabfugnic@gmail.com>
2013-01-31 22:05:12 -0500

    Made some tests for address=

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,25 @@ describe User do
     end
     it { @user.to_s.should == @user.email }
   end
+  
+  context "instance methods" do
+    
+    describe "#fullname" do
+      subject { FactoryGirl.build(:user) }
+      it "displays full name of user" do
+        subject.fullname.should eq("#{subject.first_name} #{subject.last_name}")
+      end
+    end
+    describe "#home_address" do
+      before(:each) do
+        @address1 = FactoryGirl.build(:address)
+        @address2 = FactoryGirl.build(:address, :home => true)
+      end
+      subject { FactoryGirl.create(:user, :addresses => [ @address1, @address2 ]) }
+      it "returns home address for user" do
+        subject.home_address.should eq(@address2)
+      end
+    end
+  end
 
 end
